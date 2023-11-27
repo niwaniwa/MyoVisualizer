b8d28c0fbafb57bbc27a533ca59a736d1d129c81
koke <niwaniwa@users.noreply.github.com>
2023-11-28 00:31:20 +0900

    2ch表示できるように

--- MyoVisualizer/MainWindow.xaml.cs
+++ MyoVisualizer/MainWindow.xaml.cs
@@ -30,7 +30,8 @@ namespace MyoVisualizer
     {
 
         private VisualizerAnalogProcessor _processor;
-        private ScatterSeries _scatterSeries;
+        private ScatterSeries _scatterSeries1;
+        private ScatterSeries _scatterSeries2;
         private const int viewSize = 6000;
         private DispatcherTimer timer;
         private int time;
@@ -42,11 +43,9 @@ namespace MyoVisualizer
         {
             InitializeComponent();
 
-
-            var model = new PlotModel { Title = "Frequency Data" };
-
-
-            model.Axes.Add(new LinearAxis
+            // ch.1
+            var model1 = new PlotModel { Title = "Frequency Data ch.1" };
+            model1.Axes.Add(new LinearAxis
             {
                 Position = AxisPosition.Bottom,
                 Minimum = 0,
@@ -55,10 +54,26 @@ namespace MyoVisualizer
                 AbsoluteMaximum = (2000 * 60) * 20 // 上限は10分
             });
 
-            _scatterSeries = new ScatterSeries { MarkerType = MarkerType.Circle, MarkerSize = 2  };
-            model.Series.Add(_scatterSeries);
+            _scatterSeries1 = new ScatterSeries { MarkerType = MarkerType.Circle, MarkerSize = 2  };
+            model1.Series.Add(_scatterSeries1);
+            
+            // ch.2
+            var model2 = new PlotModel { Title = "Frequency Data ch.2" };
+            model2.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Bottom,
+                Minimum = 0,
+                Maximum = viewSize,
+                AbsoluteMinimum = 0,
+                AbsoluteMaximum = (2000 * 60) * 20 // 上限は10分
+            });
 
-            Plot.Model = model;
+            _scatterSeries2 = new ScatterSeries { MarkerType = MarkerType.Circle, MarkerSize = 2  };
+            model2.Series.Add(_scatterSeries2);
+
+            // ここまで
+            Plot.Model = model1; // ch.1の軸
+            Plot2.Model = model2; // ch.2の軸
             
             _processor = new VisualizerAnalogProcessor(samplingClock, eventSamplingCount, 2);
             timer = new DispatcherTimer();
@@ -83,21 +98,30 @@ namespace MyoVisualizer
                 return;
             }
 
-            var newPoints = _processor.Data[0].Skip(count).ToList();
-            _scatterSeries.Points.AddRange(newPoints.Select((point, index) => new ScatterPoint(count + index, point)));
+            var newPoints1 = _processor.Data[0].Skip(count).ToList(); // ch.1
+            var newPoints2 = _processor.Data[1].Skip(count).ToList(); // ch.2
+            
+            _scatterSeries1.Points.AddRange(newPoints1.Select((point, index) => new ScatterPoint(count + index, point)));
+            _scatterSeries2.Points.AddRange(newPoints2.Select((point, index) => new ScatterPoint(count + index, point)));
 
-            count += newPoints.Count;
+            count += newPoints1.Count;
             
             deleteIndex = count;
             
             if (count > viewSize)
             {
+                // ch.1
                 Plot.Model.Axes[0].Minimum = count - viewSize;
                 Plot.Model.Axes[0].Maximum = count;
-                _scatterSeries.Points.RemoveRange(0, newPoints.Count);
+                _scatterSeries1.Points.RemoveRange(0, newPoints1.Count);
+                // ch.2
+                Plot.Model.Axes[1].Minimum = count - viewSize;
+                Plot.Model.Axes[1].Maximum = count;
+                _scatterSeries2.Points.RemoveRange(0, newPoints2.Count);
             }
             
             Plot.InvalidatePlot(true);
+            Plot2.InvalidatePlot(true);
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
